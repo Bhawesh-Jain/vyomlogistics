34a6750daed71cc9fc6a80e60c1f95a7d85e232c
bhawesh <bhaweshjain384@gmail.com>
2025-12-17 16:28:57 +0530

    Log Removed

--- src/lib/repositories/miscRepository.ts
+++ src/lib/repositories/miscRepository.ts
@@ -480,7 +480,6 @@ export class MiscRepository extends RepositoryBase {
         totalSpaceUtilizaton,
         companyBreakdown
       };
-      console.log(resp);
       
       return this.success(resp)
     } catch (error) {
